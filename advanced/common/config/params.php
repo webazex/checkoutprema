1467aab1779e65a1e4133f6cd617f1f0e71ee60f
webazex <webazex@gmail.com>
2026-04-10 11:30:56 +0300

    fix: set correct wfp test credentials

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -6,7 +6,7 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
-    'wayforpay.merchantAccount' => 'test_merch_n1',
-    'wayforpay.secretKey' => 'flk3409refn54t54t*FNJRET',
-    'wayforpay.domain' => 'checkoutprema.biz.ua',
+    'wayforpay.merchantAccount' => '',
+    'wayforpay.secretKey' => '',
+    'wayforpay.domain' => '',
 ];
