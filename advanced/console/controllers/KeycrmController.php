ea2409ea0fcf53912920bba61067f5e156ead1e5
webazex <webazex@gmail.com>
2026-04-20 11:12:57 +0300

    feature: add new console action

--- advanced/console/controllers/KeycrmController.php
+++ advanced/console/controllers/KeycrmController.php
@@ -8,6 +8,7 @@ use common\services\keycrm\KeyCrmProductImportService;
 use Yii;
 use yii\console\Controller;
 use yii\console\ExitCode;
+use common\services\keycrm\KeyCrmArchivedProductCleanupService;
 
 final class KeycrmController extends Controller
 {
@@ -33,6 +34,25 @@ final class KeycrmController extends Controller
         $this->stdout("Orphan repaired: {$stats['orphanRepaired']}\n");
         $this->stdout("Orphan removed: {$stats['orphanRemoved']}\n");
         $this->stdout("Errors: {$stats['errors']}\n");
+
+        return ExitCode::OK;
+    }
+
+    public function actionCleanupArchivedProducts(int $days = 7): int
+    {
+        /** @var KeyCrmArchivedProductCleanupService $service */
+        $service = \Yii::$container->get(KeyCrmArchivedProductCleanupService::class);
+
+        $stats = $service->cleanup($days > 0 ? $days : 7);
+
+        $this->stdout("KeyCRM archived products cleanup completed.\n");
+        $this->stdout('Scanned: ' . $stats['scanned'] . "\n");
+        $this->stdout('Eligible: ' . $stats['eligible'] . "\n");
+        $this->stdout('Deleted: ' . $stats['deleted'] . "\n");
+        $this->stdout('Skipped (cart items): ' . $stats['skippedCartItems'] . "\n");
+        $this->stdout('Skipped (order items): ' . $stats['skippedOrderItems'] . "\n");
+        $this->stdout('Skipped (missing archived_at): ' . $stats['skippedMissingArchivedAt'] . "\n");
+        $this->stdout('Errors: ' . $stats['errors'] . "\n");
 
         return ExitCode::OK;
     }
