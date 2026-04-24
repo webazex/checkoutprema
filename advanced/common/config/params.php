2a3bee6d7f6ba748beb9d4eec2a90aee36c69010
webazex <webazex@gmail.com>
2026-04-24 18:25:58 +0300

    update: change lang psswd

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -5,7 +5,7 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
-    'user.passwordMinLength' => 8,
+    'user.passwordMinLength' => 13,
     'wayforpay.merchantAccount' => 'freelance_user_6720e0795a509',
     'wayforpay.secretKey' => 'c97b450c4c2f68fc07aa78db72ac9b1e16f75fc1',
     'wayforpay.domain' => 'checkoutprema.biz.ua',
